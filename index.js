537a5ea91488ed169790f843cf028473601544e2
Samuel Douglas Othieno <douglasothieno@gmail.com>
2024-05-19 15:15:50 +0300

    Add Data Validation with JOI. 3 Schemas created and a Validation function.

--- index.js
+++ index.js
@@ -3,27 +3,26 @@ import morgan from 'morgan';
 import {StatusCodes} from 'http-status-codes';
 import helmet from "helmet";
 import cors from 'cors';
-import {updatePatientData,findUniquePatient, findPatients, createNewPatient, deleteAllPatients, deleteAPatient} from './Controllers/users.controller.js';
+import patientRouter from './Routers/patients.router.js';
+
 
 const App = express()
 const PORT = 5000;
 
-//! Middleware
+//! Middleware=================================================================================
 App.use(morgan('dev'))
 App.use(express.json())
 App.use(cors())
 App.use(helmet())
 
+
+
+//! Request Handlers===========================================================================
 App.get('/', (req, res)=>{
     res.status(StatusCodes.OK).json({message: "Hello, Welcome to SwiftDocUg"})
 })
 
-App.get('/user', findUniquePatient)
-App.get('/all-users', findPatients)
-App.post('/create-patient', createNewPatient)
-App.put('/update', updatePatientData)
-App.delete('/delete', deleteAPatient)
-App.delete('/delete-all', deleteAllPatients)
+App.use('/swiftdocug', patientRouter)
 
 App.listen(PORT,(req, res)=>{
     console.log(`Server is running on http://Localhost:${PORT}`)
