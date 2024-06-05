f2ed4c2a088eada9ad89784e1d939a01a9e8c462
Samuel Douglas Othieno <douglasothieno@gmail.com>
2024-06-05 15:51:27 +0300

    Send

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ import cors from 'cors';
 import patientRouter from './Routers/patients.router.js';
 
 const App = express()
-const host_phrase = process.env.host_phrase;
+const host_phrase = process.env.HOST_PHRASE;
 const PORT = process.env.PORT;
 const host = process.env.HOST;
+
 
 //! Middleware=================================================================================
 App.use(morgan('dev'))
